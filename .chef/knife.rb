d9070bafea590818a061e7e076ffa41798ca8884
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2018-08-07 17:54:09 -0300

    cant remember

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,3 +1,10 @@
+local_mode true
+chef_repo_path File.expand_path('../' , __FILE__)
+
+knife[:ssh_attribute] = "knife_zero.host"
+knife[:use_sudo] = true
+knife[:editor] = '/usr/bin/vim'
+
 cookbook_path ["cookbooks", "site-cookbooks"]
 node_path     "nodes"
 role_path     "roles"
@@ -7,3 +14,5 @@ ssl_verify_mode :verify_peer
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
+knife[:before_bootstrap] = "berks vendor"
+knife[:before_converge] = "berks vendor"
